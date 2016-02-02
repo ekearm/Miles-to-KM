1eb3d8e6d992045bc7c10eed15ce229ccac52d8e
Edric Dearment <Ed@edrics-macbook-air.local>
2016-02-02 11:48:27 -0800

    Initial Commit

---
+++ Miles to KM/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Miles to KM
+//
+//  Created by Edric Dearment on 2/2/16.
+//  Copyright © 2016 Edric Dearment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
